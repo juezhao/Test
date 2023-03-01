2b3c6436aac86afa2d9616f539a8271f45bdeb02
juezhao <nzjerryzhao@gmail.com>
2023-03-01 16:33:19 +1300

    1

--- Website.php
+++ Website.php
@@ -1,37 +1,22 @@
 <?php
-// Set API endpoint and version
-$api_url = 'https://api.demo.sitehost.co.nz/v1.2/';
 
-// Set API key and customer ID
-$api_key = 'd17261d51ff7046b760bd95b4ce781ac';
-$customer_id = 293785;
+// Set up API endpoint URL with SSH authentication
+$url = 'https://jz20230301:nB1Xyx9P@api.demo.sitehost.co.nz/v1.0/clients/293785/domains';
 
-// Build API request URL
-$request_url = $api_url . 'customers/' . $customer_id . '/domains.json';
+// Send API request and get response
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
 
-// Set headers for API request
-$headers = array(
-    'Authorization: Bearer ' . $api_key,
-    'Content-Type: application/json'
-);
-
-// Make API request using cURL
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $request_url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers
-));
-$response = curl_exec($curl);
-curl_close($curl);
-
-// Parse API response
+// Parse response into an array
 $domains = json_decode($response, true);
 
-// Output domains on web page
+// Output domains as an HTML list
 echo '<ul>';
 foreach ($domains as $domain) {
-    echo '<li>' . $domain['name'] . '</li>';
+  echo '<li>' . $domain['name'] . '</li>';
 }
 echo '</ul>';
+
 ?>
