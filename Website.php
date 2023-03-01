10fd0c7c5a94fdba762ec396f1a8983dd6c1ba66
juezhao <nzjerryzhao@gmail.com>
2023-03-01 16:19:25 +1300

    Json

--- Website.php
+++ Website.php
@@ -1,18 +1,18 @@
 <?php
 // Set API endpoint and version
-$api_url = 'https://api.demo.sitehost.co.nz/v1.2-public/';
+$api_url = 'https://api.demo.sitehost.co.nz/v1.0-public/';
 
 // Set API key and customer ID
 $api_key = 'd17261d51ff7046b760bd95b4ce781ac';
 $customer_id = 293785;
 
 // Build API request URL
-$request_url = $api_url . 'customers/' . $customer_id . '/domains.xml';
+$request_url = $api_url . 'customers/' . $customer_id . '/domains.json';
 
 // Set headers for API request
 $headers = array(
     'Authorization: Bearer ' . $api_key,
-    'Content-Type: application/xml'
+    'Content-Type: application/json'
 );
 
 // Make API request using cURL
@@ -26,15 +26,12 @@ $response = curl_exec($curl);
 curl_close($curl);
 
 // Parse API response
-$domains = simplexml_load_string($response);
+$domains = json_decode($response, true);
 
 // Output domains on web page
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<domains>';
-foreach ($domains->domain as $domain) {
-    echo '<domain>';
-    echo '<name>' . $domain->name . '</name>';
-    echo '</domain>';
+echo '<ul>';
+foreach ($domains as $domain) {
+    echo '<li>' . $domain['name'] . '</li>';
 }
-echo '</domains>';
+echo '</ul>';
 ?>
